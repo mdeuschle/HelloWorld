97bf07aa6a917a3cbf6b8f05625ef758d71c211f
Matt Deuschle <mdeuschle@gmail.com>
2026-04-19 16:46:43 -0700

    Initial commit: add app files

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Matt Deuschle on 4/19/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
